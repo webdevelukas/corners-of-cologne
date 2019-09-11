7ec8872c4591acc9d1e9d6c67a9768fc1560e47d
Lukas Kreidenweis <lukas@deinklub.de>
2019-09-11 18:49:27 +0200

    modified colors

--- src/components/FilterList.js
+++ src/components/FilterList.js
@@ -4,6 +4,8 @@ import Filter from "./Filter";
 function FilterList() {
   return (
     <section className="filterlist">
+      <i class="fas fa-filter"></i>
+      <Filter />
       <Filter />
       <Filter />
       <Filter />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Header() {
-  return <div className="header">Corners Of Cologne</div>;
+  return (
+    <div className="header">
+      <h1 className="header__logo">Corners Of Cologne</h1>
+    </div>
+  );
 }
 
 export default Header;
