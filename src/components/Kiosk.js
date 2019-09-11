0540e07ef932a54b52ab0855886eb86e04a45115
Lukas Kreidenweis <lukas@deinklub.de>
2019-09-11 16:30:18 +0200

    add responsive images

--- src/components/Kiosk.js
+++ src/components/Kiosk.js
@@ -5,8 +5,10 @@ function Kiosk() {
     <article className="kiosk">
       <div className="kiosk__image"></div>
       <div>
-        <h3 className="kiosk__name">Kiosk name</h3>
-        <p className="kiosk__description">Kiosk description</p>
+        <h3 className="kiosk__name">Dreikönigen-Büdchen</h3>
+        <p className="kiosk__description">
+          Ecke Anno-/Dreikönigenstraße, tgl. 6-23 Uhr
+        </p>
       </div>
     </article>
   );
