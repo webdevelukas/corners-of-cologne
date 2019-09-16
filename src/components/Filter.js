8cdb9468566c77e35d804077126a5205209fcf68
Lukas Kreidenweis <lukas@deinklub.de>
2019-09-16 16:33:30 +0200

    add onFilterChange event

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,7 +2,12 @@ import React from "react";
 
 function Filter(props) {
   return (
-    <select className="filterlist__filter">
+    <select
+      className="filterlist__filter"
+      onChange={event => {
+        props.onChange(props.filter.name, event.target.value);
+      }}
+    >
       <option>{props.filter.name}</option>
       {props.filter.options.map(option => {
         return (
