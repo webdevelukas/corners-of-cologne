350dee120186d849600ed98010d6f5a244d0c033
Lukas Kreidenweis <lukas@deinklub.de>
2019-09-17 17:22:07 +0200

    reset filter by return state

--- src/components/KiosksList.js
+++ src/components/KiosksList.js
@@ -5,6 +5,72 @@ import { kiosks } from "../api/kiosks";
 function KiosksList({ selectedFilters, onFilterChange }) {
   console.log(selectedFilters);
   const filteredKiosks = kiosks.filter(kiosk => {
+    // Filter distance
+    if (selectedFilters.distance) {
+      switch (selectedFilters.distance) {
+        case "< 2 min":
+          if (kiosk.distance > 2) {
+            return false;
+          }
+          break;
+        case "< 5 min":
+          if (kiosk.distance > 5) {
+            return false;
+          }
+          break;
+        case "< 10 min":
+          if (kiosk.distance > 10) {
+            return false;
+          }
+          break;
+        default:
+          return selectedFilters;
+      }
+    }
+
+    // Filter rating
+    if (selectedFilters.rating) {
+      if (selectedFilters.rating === "rating") {
+        return selectedFilters;
+      }
+      if (kiosk.rating <= selectedFilters.rating.length) {
+        return false;
+      }
+    }
+
+    // Filter category
+    if (selectedFilters.category) {
+      if (selectedFilters.category === "category") {
+        return selectedFilters;
+      }
+      if (!kiosk.categories.includes(selectedFilters.category)) {
+        return false;
+      }
+    }
+
+    // Filter price
+    if (selectedFilters.price) {
+      switch (selectedFilters.price) {
+        case "low":
+          if (kiosk.price >= 1.5) {
+            return false;
+          }
+          break;
+        case "middle":
+          if (kiosk.price >= 2.2) {
+            return false;
+          }
+          break;
+        case "high":
+          if (kiosk.price < 2.2) {
+            return false;
+          }
+          break;
+        default:
+          return selectedFilters;
+      }
+    }
+
     return true;
   });
 
